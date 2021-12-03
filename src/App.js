94024340ca68ec99bb47ffb51b8b659f94f633c9
MDaud32 <theprofessor543@gmail.com>
2021-12-03 01:13:42 -0800

    shuffle cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import SingleCard from './components/SingleCard';
+
+const cardsImages = [
+  { src: '/img/helmet-1.png' },
+  { src: '/img/potion-1.png' },
+  { src: '/img/ring-1.png' },
+  { src: '/img/scroll-1.png' },
+  { src: '/img/shield-1.png' },
+  { src: '/img/sword-1.png' },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turn, setTurn] = useState(0);
+
+  const sheffleCard = () => {
+    const sheffledCards = [...cardsImages, ...cardsImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(sheffledCards);
+    setTurn(0);
+  };
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={sheffleCard}>New Game</button>
+
+      <div className='card-grid'>
+        {cards.map((card) => (
+          <SingleCard card={card} key={card.id} />
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
